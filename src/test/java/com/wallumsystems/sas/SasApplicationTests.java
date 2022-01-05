8443e68b4352d6d28bccde2e755d0ced78a9da3a
BenchmarkingBuffalo <l.wulff@wallum-systems.com>
2022-01-05 21:29:54 +0100

    Created an empty project.

---
+++ src/test/java/com/wallumsystems/sas/SasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wallumsystems.sas;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SasApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true, "Is not true");
+    }
+
+}
