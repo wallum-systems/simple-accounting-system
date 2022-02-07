3974c22c9fce9a90ef18b44fc2fb456b630c093c
BenchmarkingBuffalo <l.wulff@wallum-systems.com>
2022-02-07 21:25:16 +0100

    Added CORS for the whole API

--- src/main/java/com/wallumsystems/sas/SasApplication.java
+++ src/main/java/com/wallumsystems/sas/SasApplication.java
@@ -22,12 +22,25 @@ package com.wallumsystems.sas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SasApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
